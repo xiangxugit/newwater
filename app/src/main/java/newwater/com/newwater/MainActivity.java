b610af2de3a11d22f8de429800f8eb607e4c442d
xiangxugit <568173020@qq.com>
2018-04-15 01:01:13 +0800

    增加倒计时
    增加倒计时

--- app/src/main/java/newwater/com/newwater/MainActivity.java
+++ app/src/main/java/newwater/com/newwater/MainActivity.java
@@ -2,12 +2,29 @@ package newwater.com.newwater;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    public void initView(){
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.exit:
+                //
+                break;
+        }
+
     }
 }
--- app/src/main/java/newwater/com/newwater/utils/TimeBack.java
+++ app/src/main/java/newwater/com/newwater/utils/TimeBack.java
@@ -1,8 +1,51 @@
 package newwater.com.newwater.utils;
 
-/**
- * Created by Administrator on 2018\4\15 0015.
- */
+import android.os.CountDownTimer;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class TimeBack {
+    //timecount
+    private TimeCount timecount;
+    private TextView showTextView;
+    private long millissInFuture;
+    private long countDownInterval;
+    public TimeBack(TextView showTextView,long millisInFuture,long countDownInterval)
+    {
+        this.showTextView = showTextView;
+        this.timecount =  new TimeCount(millisInFuture, countDownInterval);
+        timecount.start();
+		/*showTextView.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});*/
+    }
+    public class TimeCount extends CountDownTimer {
+
+        public TimeCount(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+            // TODO Auto-generated constructor stub
+
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            // TODO Auto-generated method stub
+            showTextView.setClickable(false);
+            showTextView.setText(millisUntilFinished/1000+"秒");
+        }
+
+        @Override
+        public void onFinish() {
+            // TODO Auto-generated method stub
+            showTextView.setText("重新验证");
+            showTextView.setClickable(true);
+        }
+
+    }
 }
